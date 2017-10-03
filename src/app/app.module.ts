15ffd432f983986b1aa40d047f554c82ba82e623
BRYAN <bryanap3294@gmail.com>
2017-10-03 10:30:42 -0500

    correccion merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-<<<<<<< HEAD
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-=======
 import {AuthModule} from './auth/auth.module';
->>>>>>> 1d7f9b4a2a84e62629804463697883726f5e3c3f
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    SignupComponent,
-    SigninComponent
-=======
->>>>>>> 1d7f9b4a2a84e62629804463697883726f5e3c3f
   ],
   imports: [
     BrowserModule,
