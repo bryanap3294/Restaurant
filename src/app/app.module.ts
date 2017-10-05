f4ddfb2936cbc8da818782fa2587ec13623c4a5b
Bryan Cesar Aguilar Perez <Bryan@MacBook-Pro-de-Bryan.local>
2017-10-04 23:25:34 -0500

    componentes de los roles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import {AuthModule} from './auth/auth.module';
+import { ManagerComponent } from './rol/manager/manager.component';
+import { ChefComponent } from './rol/chef/chef.component';
+import { CashierComponent } from './rol/cashier/cashier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ManagerComponent,
+    ChefComponent,
+    CashierComponent,
   ],
   imports: [
     BrowserModule,
