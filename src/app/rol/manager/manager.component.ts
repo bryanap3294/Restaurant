4ff233ef8e736d60dc548a027a2dd19be8a85cbf
Bryan Cesar Aguilar Perez <Bryan@MacBook-Pro-de-Bryan.local>
2017-11-02 10:02:10 -0500

    comenzando con manager

--- src/app/rol/manager/manager.component.ts
+++ src/app/rol/manager/manager.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../../auth/auth.service';
+import { DataStorageService } from '../../shared/data-storage.service';
+import { OrdenService } from '../../model/orden/orden.service';
+import { Orden } from '../../model/orden/orden.model';
 
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
-export class ManagerComponent implements OnInit {
+export class ManagerComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Orden;
 
-  constructor() { }
+  ordenes: Orden[];
+
+  constructor(private ordenService: OrdenService,
+    private authService: AuthService,
+    private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      this.dataStorageService.getOrdenes();
+      this.ordenes = this.ordenService.getOrdenes();
+      this.subscription = this.ordenService.ordenChanged
+        .subscribe(
+        (ordenes: Orden[]) => {
+          this.ordenes = ordenes;
+        }
+        );
+    }
+    this.subscription = this.ordenService.startedEditing
+      .subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.ordenService.getOrden(index);
+        this.slForm.setValue({
+          name: this.editedItem.nombreCliente,
+          monto: this.editedItem.monto,
+          estado: this.editedItem.estado,
+          platos: this.editedItem.platos
+        })
+      }
+      );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
